0e2e40a85046f05136df2da1e02f493c80ddd048
rstrzalkowski <resumc@wp.pl>
2023-07-13 21:36:43 +0200

    Initial commit

---
+++ src/main/java/pl/lodz/p/it/rstrzalkowski/syllabus/CqrsEsSyllabusApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.rstrzalkowski.syllabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsEsSyllabusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsEsSyllabusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lodz/p/it/rstrzalkowski/syllabus/CqrsEsSyllabusApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.rstrzalkowski.syllabus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsEsSyllabusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
